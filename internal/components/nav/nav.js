212a0eabaf78b1c59bff49895d57f58cf9451b9b
hartsfield <johnathanhartsfield@gmail.com>
2023-08-19 15:51:05 -0400

    updates

--- internal/components/nav/nav.js
+++ internal/components/nav/nav.js
@@ -5,10 +5,12 @@ function togglePostForm() {
         document.getElementById("section-submitForm").style.display = "block";
         document.getElementById("newPostButt").innerHTML = "-";
         document.getElementById("newPostButt").style.background = "#8d561f";
+        document.getElementById("newPostButt").style.border = "1px solid #6b3000";
     } else {
         document.getElementById("section-submitForm").style.display = "none";
-        document.getElementById("newPostButt").innerHTML = "★";
+        document.getElementById("newPostButt").innerHTML = "+";
         document.getElementById("newPostButt").style.background = "#9cd4d5";
+        document.getElementById("newPostButt").style.border = "1px solid #538d99";
     }
 
 }
